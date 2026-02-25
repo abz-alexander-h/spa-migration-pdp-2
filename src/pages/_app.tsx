9c37d2f315227686c0b304d2ba8532f4f4df35ba
Frontend_Alexander_H <alexander.holub@abz.agency>
2026-02-25 10:14:32 +0200

    refactor: remove `SpeedInsights` from `_app` as it is no longer needed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,11 @@ import type { AppProps } from 'next/app';
 import { MainLayout } from '@/widgets/layout/main-layout/ui/main-layout';
 
 import '@/app-layer/styles/globals.css';
-import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <MainLayout>
       <Component {...pageProps} />
-      <SpeedInsights />
     </MainLayout>
   );
 }
